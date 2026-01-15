a6fa49c61039af04828ff065dce4b8f7350f155f
Prabhat Joshi <prabhat.joshi.00000@gmail.com>
2026-01-15 19:34:21 +0530

    Add Admin-Dashboard Page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import Login from './pages/login.jsx';
+import AdminDashboard from './pages/admin-dashboard.jsx';
 import { Routes, Route } from 'react-router';
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Login />} />  
+        <Route path="/" element={<Login />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />} /> 
       </Routes>
     </>
   );
